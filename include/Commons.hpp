f2faf0273b54c342765c729c2489d24682757365
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-04-27 19:26:52 +0800

    Added licenses

--- include/Commons.hpp
+++ include/Commons.hpp
@@ -1,16 +1,33 @@
+/*
+ *  Commons.hpp - Common type shortcuts used in the program
+ *  Copyright (C) 2022 Everett Gaius S. Vergara
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *	
+ */
+
 #ifndef _COMMONS_HPP_
 #define _COMMONS_HPP_
 
 #include <SDL.h>
 
 namespace g80 {
-    using Dim = Sint32;
     using Dim32 = Sint32;
     using Dim16 = Sint16;
     using Color = Uint32;
     using AngleIx = Sint16;
     using RandNum = Uint32;
-
 }
 
 #endif 
